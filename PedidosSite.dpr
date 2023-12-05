cc832954303881736c57cfe49e0dd77173ba4c20
ShoemixDev <informatica@shoemix.com.br>
2023-12-05 12:10:55 -0300

    filipe

---
+++ PedidosSite.dpr
@@ -0,0 +1,15 @@
+program PedidosSite;
+
+uses
+  Vcl.Forms,
+  UAtualizador in 'UAtualizador.pas' {Form2};
+
+{$R *.res}
+
+begin
+ // ReportMemoryLeaksOnShutdown :=true;
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
